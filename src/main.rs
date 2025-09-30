b1b5abc14f105e4b8ca7912af71b31209715ebb1
ArturSayf <artursayf7@gmail.com>
2025-09-30 20:25:16 +0500

    added grid movement and turns

--- src/main.rs
+++ src/main.rs
@@ -45,69 +45,51 @@ use crate::Side_of_the_world::*;
                 };
             },
             "turn right" => {
-                if character.side_of_the_world == Side_of_the_world::North {
-                    character.side_of_the_world = Side_of_the_world::East;
-                } else if character.side_of_the_world == Side_of_the_world::South {
-                    character.side_of_the_world = Side_of_the_world::West;
-                } else if character.side_of_the_world == Side_of_the_world::East {
-                    character.side_of_the_world = Side_of_the_world::South;
-                } else if character.side_of_the_world == Side_of_the_world::West {
-                    character.side_of_the_world = Side_of_the_world::North;
-                }
+                character.side_of_the_world = match character.side_of_the_world {
+                    North => East,
+                    South => West,
+                    East => South,
+                    West => North,
+                };
             },
             "turn around" => {
-                if character.side_of_the_world == Side_of_the_world::North {
-                    character.side_of_the_world = Side_of_the_world::South;
-                } else if character.side_of_the_world == Side_of_the_world::South {
-                    character.side_of_the_world = Side_of_the_world::North;
-                } else if character.side_of_the_world == Side_of_the_world::East {
-                    character.side_of_the_world = Side_of_the_world::West;
-                } else if character.side_of_the_world == Side_of_the_world::West {
-                    character.side_of_the_world = Side_of_the_world::East;
-                }
+                character.side_of_the_world = match character.side_of_the_world {
+                    North => South,
+                    South => North,
+                    East => West,
+                    West => East,
+                };
             },
             "forward" => {
-                if character.side_of_the_world == Side_of_the_world::North {
-                    character.y += 1;
-                } else if character.side_of_the_world == Side_of_the_world::South {
-                    character.y -= 1;
-                } else if character.side_of_the_world == Side_of_the_world::East {
-                    character.x += 1;
-                } else if character.side_of_the_world == Side_of_the_world::West {
-                    character.x -= 1;
+                match character.side_of_the_world {
+                    Side_of_the_world::North => character.y += 1,
+                    Side_of_the_world::South => character.y -= 1,
+                    Side_of_the_world::East => character.x += 1,
+                    Side_of_the_world::West => character.x -= 1,
                 }
             },
             "back" => {
-                if character.side_of_the_world == Side_of_the_world::North {
-                    character.y -= 1;
-                } else if character.side_of_the_world == Side_of_the_world::South {
-                    character.y += 1;
-                } else if character.side_of_the_world == Side_of_the_world::East {
-                    character.x -= 1;
-                } else if character.side_of_the_world == Side_of_the_world::West {
-                    character.x += 1;
+                match character.side_of_the_world {
+                    Side_of_the_world::North => character.y -= 1,
+                    Side_of_the_world::South => character.y += 1,
+                    Side_of_the_world::East => character.x -= 1,
+                    Side_of_the_world::West => character.x += 1,
                 }
             },
             "left" => {
-                if character.side_of_the_world == Side_of_the_world::North {
-                    character.x -= 1;
-                } else if character.side_of_the_world == Side_of_the_world::South {
-                    character.x += 1;
-                } else if character.side_of_the_world == Side_of_the_world::East {
-                    character.y += 1;
-                } else if character.side_of_the_world == Side_of_the_world::West {
-                    character.y -= 1;
+                match character.side_of_the_world {
+                    Side_of_the_world::North => character.x -= 1,
+                    Side_of_the_world::South => character.x += 1,
+                    Side_of_the_world::East => character.y += 1,
+                    Side_of_the_world::West => character.y -= 1,
                 }
             },
             "right" => {
-                if character.side_of_the_world == Side_of_the_world::North {
-                    character.x += 1;
-                } else if character.side_of_the_world == Side_of_the_world::South {
-                    character.x -= 1;
-                } else if character.side_of_the_world == Side_of_the_world::East {
-                    character.y -= 1;
-                } else if character.side_of_the_world == Side_of_the_world::West {
-                    character.y += 1;
+                match character.side_of_the_world {
+                    Side_of_the_world::North => character.x += 1,
+                    Side_of_the_world::South => character.x -= 1,
+                    Side_of_the_world::East => character.y -= 1,
+                    Side_of_the_world::West => character.y += 1,
                 }
             },
             _ => println!("Неверная команда!"),
