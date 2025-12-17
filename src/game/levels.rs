d7831a77342ef9d1d85d45635693b2f22dab3e33
student <student@usue.ru>
2025-12-17 13:17:04 +0500

    added items to levels

--- src/game/levels.rs
+++ src/game/levels.rs
@@ -27,8 +27,9 @@ pub const PAPER_NOTE_12: &str = "................Аттестат...............
 Биология....................4 (хорошо) 
 Химия.......................4 (хорошо) 
 Физическая_культура.........5 (отлично)";
-pub const PAPER_NOTE_13: &str = "..............Объявление!.............. Студенты, не сдавшие сессию, повешены на третьем этаже главного корпуса.";
-pub const PAPER_NOTE_14: &str = "..............Объявление!.............. Студенты, не сдающие верхнюю одежду в гардероб, будут повешены на третьем этаже гланого корпуса.";
+pub const PAPER_NOTE_13: &str = "..............Объявление!..............\0\0 Студенты, не сдавшие сессию, повешены на третьем этаже главного корпуса.";
+pub const PAPER_NOTE_14: &str = "..............Объявление!..............\0\0 Студенты, не сдающие верхнюю одежду в гардероб, будут повешены на третьем этаже гланого корпуса.";
+pub const PAPER_NOTE_15: &str = "Код от сейфа: (5!+6!)*4/3-98";
 
 #[derive(Clone)]
 pub struct Level {
@@ -56,12 +57,13 @@ impl Level {
         let paper_note_12 = PAPER_NOTE_12.to_string();
         let paper_note_13 = PAPER_NOTE_13.to_string();
         let paper_note_14 = PAPER_NOTE_14.to_string();
+        let paper_note_15 = PAPER_NOTE_15.to_string();
 
         let (field, player_x, player_y, player_dir, enemies) = match number {
             1 => Self::create_level_1(&paper_note_1, &paper_note_2, &paper_note_3, &paper_note_4),
             2 => Self::create_level_2(&paper_note_5, &paper_note_6, &paper_note_7, &paper_note_8, &paper_note_9, &paper_note_10),
             3 => Self::create_level_3(&paper_note_11, &paper_note_12),
-            4 => Self::create_level_4(&paper_note_13, &paper_note_14),
+            4 => Self::create_level_4(&paper_note_13, &paper_note_14, &paper_note_15),
             _ => Self::create_level_1(&paper_note_1, &paper_note_2, &paper_note_3, &paper_note_4),
         };
 
@@ -154,27 +156,27 @@ impl Level {
         (field, 18, 9, SideOfTheWorld::South, enemies)
     }
 
-    fn create_level_4(paper_note_13: &str, paper_note_14: &str) -> 
+    fn create_level_4(paper_note_13: &str, paper_note_14: &str, paper_note_15: &str) -> 
         ([[Cell; FIELD_WIDTH]; FIELD_HEIGHT], usize, usize, SideOfTheWorld, Vec<crate::game::Enemy>) {
         
         let field: [[Cell; FIELD_WIDTH]; FIELD_HEIGHT] = [
-            [Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall],
-            [Cell::Wall, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Wall],
-            [Cell::Wall, Cell::Pass, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Pass, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Pass, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Pass, Cell::Wall],
-            [Cell::Wall, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Wall],
-            [Cell::Wall, Cell::Pass, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Pass, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Pass, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Pass, Cell::Wall],
-            [Cell::Wall, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Wall],
-            [Cell::Wall, Cell::Pass, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Pass, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Pass, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Pass, Cell::Wall],
-            [Cell::Wall, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Wall],
-            [Cell::Wall, Cell::Pass, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Pass, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Pass, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Pass, Cell::Wall],
-            [Cell::Wall, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Wall],
-            [Cell::Wall, Cell::Pass, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Pass, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Pass, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Pass, Cell::Wall],
-            [Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall],
-        ];
+    [Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall],
+    [Cell::Wall, Cell::Pass, Cell::Wall, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Wall, Cell::Pass, Cell::LiftingGates { state: false, number: 3, direction: SideOfTheWorld::West }, Cell::Pass, Cell::Wall, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Wall, Cell::Pass, Cell::LiftingGates { state: false, number: 5, direction: SideOfTheWorld::West }, Cell::Pass, Cell::Wall, Cell::Pass, Cell::Pass, Cell::Exit { state: false, direction: SideOfTheWorld::East }],
+    [Cell::Wall, Cell::Pass, Cell::Wall, Cell::LiftingGates { state: false, number: 2, direction: SideOfTheWorld::North }, Cell::Wall, Cell::Pass, Cell::Wall, Cell::Pass, Cell::Wall, Cell::Pass, Cell::Wall, Cell::Pass, Cell::Wall, Cell::LiftingGates { state: false, number: 4, direction: SideOfTheWorld::North }, Cell::Wall, Cell::Pass, Cell::Wall, Cell::Pass, Cell::Wall, Cell::Door { direction: SideOfTheWorld::North, number: 5, state: false }, Cell::Wall, Cell::Wall],
+    [Cell::Toggle { state: false, number: 1, direction: SideOfTheWorld::East }, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Wall, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Wall, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Wall, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Wall, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Wall, Cell::Wall],
+    [Cell::Wall, Cell::LiftingGates { state: false, number: 1, direction: SideOfTheWorld::North }, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Door { direction: SideOfTheWorld::South, number: 1, state: false }, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Door { direction: SideOfTheWorld::South, number: 2, state: false }, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Door { direction: SideOfTheWorld::South, number: 3, state: false }, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Door { direction: SideOfTheWorld::South, number: 4, state: false }, Cell::Wall, Cell::Wall],
+    [Cell::Wall, Cell::Pass, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Pass, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Wall, Cell::Pass, Cell::Pass, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Pass, Cell::Pass, Cell::Wall],
+    [Cell::Wall, Cell::Pass, Cell::Pass, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Pass, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Paper { text: paper_note_15.to_string() }, Cell::Wall, Cell::Pass, Cell::Wall, Cell::Wall, Cell::Pass, Cell::Pass, Cell::Wall, Cell::Box { items: vec![Item::Key(4)] }, Cell::Wall, Cell::Pass, Cell::Wall],
+    [Cell::Wall, Cell::Wall, Cell::Pass, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Wall, Cell::Pass, Cell::Wall, Cell::Pass, Cell::Pass, Cell::Box { items: vec![Item::Key(3)] }, Cell::Wall, Cell::Pass, Cell::Wall, Cell::Pass, Cell::Wall, Cell::Pass, Cell::Wall],
+    [Cell::Wall, Cell::Wall, Cell::Pass, Cell::Wall, Cell::Wall, Cell::Box { items: vec![Item::Paper(paper_note_14.to_string()), Item::Key(2)] }, Cell::Wall, Cell::Wall, Cell::Pass, Cell::Wall, Cell::Pass, Cell::Wall, Cell::Wall, Cell::Pass, Cell::Wall, Cell::Wall, Cell::Pass, Cell::Wall, Cell::Pass, Cell::Wall, Cell::Pass, Cell::Wall],
+    [Cell::Wall, Cell::Wall, Cell::Pass, Cell::Toggle { state: false, number: 2, direction: SideOfTheWorld::West }, Cell::Wall, Cell::Pass, Cell::Wall, Cell::Wall, Cell::Pass, Cell::Wall, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Wall, Cell::Wall, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Wall, Cell::Pass, Cell::Wall],
+    [Cell::Wall, Cell::Box { items: vec![Item::Paper(paper_note_13.to_string()), Item::Key(1)] }, Cell::Pass, Cell::Pass, Cell::Wall, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Pass, Cell::Wall, Cell::Wall, Cell::Toggle { state: false, number: 4, direction: SideOfTheWorld::North }, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Toggle { state: false, number: 5, direction: SideOfTheWorld::North }, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Safe { state: false, direction: SideOfTheWorld::South, password: 2142, items: vec![Item::Key(5)] }, Cell::Wall],
+    [Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Toggle { state: false, number: 3, direction: SideOfTheWorld::North }, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall, Cell::Wall],
+];
 
         let enemies = vec![];
 
-        (field, 1, 1, SideOfTheWorld::East, enemies)
+        (field, 1, 1, SideOfTheWorld::South, enemies)
     }
 
     pub fn get_all_levels() -> Vec<Level> {
